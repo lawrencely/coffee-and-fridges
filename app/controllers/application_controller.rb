c3bb07a52ca6e9faaefdc85db8d1666b98808884
Lawrence Ly <lawrence.ly667@gmail.com>
2018-11-06 19:07:05 +0700

    users and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  before_action :authenticate
+
+private
+
+  def authenticate
+    @current_user = User.find_by(:id => session[:user_id])
+    session[:user_id] = nil unless @current_user.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   resources :locations
   resources :order_items
   resources :pages
+
+  root to: 'pages#home'
+  get '/users/edit' => 'users#edit', :as => :edit_user
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
 end
