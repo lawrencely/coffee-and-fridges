92f3dbd1b21135c168c51d7ca08cee76ffb80ef5
Lawrence Ly <lawrence.ly667@gmail.com>
2018-11-06 19:13:19 +0700

    added uniqueness on emails and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, :presence => true
-  validates :email, :presence => true
+  validates :name, :presence => true, uniqueness: true
+  validates :email, :presence => true, uniqueness: true
   validates_length_of :password, :in => 6..20, :on => :create
 end
